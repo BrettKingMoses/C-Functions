ccdef59738d2b6739f53f18bb90d216bfe273503
brett king <brettking96@outlook.com>
2017-08-17 16:04:06 +1000

    Did Ball stuff.

---
+++ Assets/6-Collisions/Scripts/MuzzleFlash.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MuzzleFlash : MonoBehaviour
+{
+    public int maxFrames = 1;
+    private int currentFrames;
+    void Update()
+    {
+        if(currentFrames >= maxFrames)
+        {
+            Destroy(gameObject);
+        }
+        currentFrames++;
+    }
+}
