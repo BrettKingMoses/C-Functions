956dd4a12054834e8e32ea2e0c821344cef00fb0
brett king <brettking96@outlook.com>
2017-08-22 11:53:42 +1000

    Continued work on Breakout.

---
+++ Assets/Breakout/Scripts/Paddle.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+namespace Breakout
+{
+    public class Paddle : MonoBehaviour
+    {
+        public float movementSpeed = 20f;
+            public Ball currentBall;
+        public Vector2[] directions;
+        // Use this for initialization
+        void Start()
+        {
+            currentBall = GetComponentInChildren<Ball>();
+        }
+        void Fire()
+        {
+            currentBall.transform.SetParent(null);
+            Vector3 randomDir = directions[Random.Range(0, directions.Length)];
+            currentBall.Fire(randomDir);
+        }
+        void CheckInput()
+        {
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                Fire();
+            }
+        }
+        void Movement()
+        {
+            float inputH = Input.GetAxis("Horizontal");
+            Vector3 force = transform.right * inputH;
+            force *= movementSpeed;
+            force *= Time.deltaTime;
+            transform.position += force;
+        }
+        // Update is called once per frame
+        void Update()
+        {
+            CheckInput();
+            Movement();
+        }
+    }
+}
